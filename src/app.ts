58fe68b75566c6d28f52da92ebd5d53a5c1cbb83
Ha Hai Binh <hhbinh@ppj-international.com>
2025-10-26 21:31:53 +0700

    add file .env

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
 // const express = require('express')
 
 import express from 'express'
+import 'dotenv/config'
 const app = express()
-const port = 6868
+const port = process.env.PORT || 6868
 
 app.get('/', (req, res) => {
-  res.send('Hello World @@!')
+  res.send('Hello World ^^^!')
 })
 
 app.get('/hhb', (req, res) => {
-  res.send('Hello HHB!')
+  res.send('Hello HHB ^^^!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log('log port', process.env.PORT)
 })
