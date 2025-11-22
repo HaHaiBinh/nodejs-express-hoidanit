0b05e1c6d200ea230aabb6080f3653da3b86ee04
Ha Hai Binh <hhbinh@ppj-international.com>
2025-11-22 10:14:51 +0700

    demo upload file video 81

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,6 +1,9 @@
 import express, { Express } from 'express'
 import { getCreateUserPage, getHomePage, getViewUserPage, postCreateUserPage, postDeleteUserPage, postUpdateUserPage } from 'controllers/user.controller'
 import { getAdminOrderPage, getAdminProductPage, getAdminUserPage, getDashboardPage } from 'controllers/admin/dashboard.controller'
+
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const router = express.Router()
 
 const webRoutes = (app: Express) => {
@@ -15,8 +18,12 @@ const webRoutes = (app: Express) => {
     router.get('/admin', getDashboardPage)
     router.get('/admin/user', getAdminUserPage)
     router.get('/admin/create-user', getCreateUserPage)
-    router.post('/admin/handle-create-user', postCreateUserPage)
-    router.get('/admin/order', getAdminOrderPage)
+    // router.post('/admin/handle-create-user', postCreateUserPage)
+    router.post('/admin/handle-create-user', upload.single('avatar'), (req, res) => {
+        return res.send('File uploaded successfully')
+    }),
+
+        router.get('/admin/order', getAdminOrderPage)
     router.get('/admin/product', getAdminProductPage)
 
     app.use('/', router)
