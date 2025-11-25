a85598df6da8ae706f597927c6d1007871f5c28f
Ha Hai Binh <hhbinh@ppj-international.com>
2025-11-25 14:49:25 +0700

    validation create product video 97

--- src/controllers/admin/product.controllers.ts
+++ src/controllers/admin/product.controllers.ts
@@ -1,10 +1,45 @@
 import { Request, Response } from 'express';
+import { ProductSchema, TProductSchema } from 'src/validation/product.schema';
 
 const getAdminCreateProductPage = async (req: Request, res: Response) => {
-    return res.render('admin/product/create.ejs');
+    const errors = [];
+    const oldData  = {
+        name: "",
+        price: "",
+        detailDesc: "",
+        shortDesc: "",
+        quantity: "",
+        factory: "",
+        target: "",
+    }
+    return res.render('admin/product/create.ejs', { errors, oldData });
 }
 
 const postCreateProductPage = async (req: Request, res: Response) => {
+
+    const { name, price, detailDesc, shortDesc, quantity, factory, target } = req.body as TProductSchema;
+
+    const validate = ProductSchema.safeParse(req.body);
+
+    if (!validate.success) {
+
+        // error
+        const errorsZod = validate.error.issues;
+        const errors = errorsZod?.map((item) => `${item.message} (${item.path[0]})`);
+        const oldData  = {
+            name,
+            price,  
+            detailDesc,
+            shortDesc,
+            quantity,
+            factory,
+            target,
+        }
+        return res.render('admin/product/create.ejs', { errors, oldData });
+    }
+
+    // success
+
     return res.redirect('/admin/product');
 }
 
---
+++ src/validation/product.schema.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+
+export const ProductSchema = z.object({
+    name: z.string().trim().min(1, { message: "Name không được để trống" }),
+    price: z.string()
+        .transform((val) => (val === "" ? 0 : Number(val)))
+        .refine((num) => num > 0, {
+            message: "Số tiền tối thiểu là 1",
+        }),
+    detailDesc: z.string().trim().min(1, { message: "Detail description không được để trống" }),
+    shortDesc: z.string().trim().min(1, { message: "Short description không được để trống" }),
+    quantity: z.string()
+        .transform((val) => (val === "" ? 0 : Number(val)))
+        .refine((num) => num > 0, {
+            message: "Quantity tối thiểu là 1",
+        }),
+
+    factory: z.string().trim().min(1, { message: "Factory không được để trống" }),
+    target: z.string().trim().min(1, { message: "Target không được để trống" }),
+})
+
+export type TProductSchema = z.infer<typeof ProductSchema>
