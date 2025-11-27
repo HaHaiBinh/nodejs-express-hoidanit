d34e0a88ec3d6a9ba8f974d9363bf68efad99717
Ha Hai Binh <hhbinh@ppj-international.com>
2025-11-27 15:05:37 +0700

    update cart video 120

--- src/services/client/item.service.ts
+++ src/services/client/item.service.ts
@@ -28,6 +28,69 @@ const addProductToCart = async (quantity: number, productId: number, user: User)
     if (cart) {
         // update existing cart item or add new one
 
+        // cập nhật sum giỏ hàng
+        await prisma.cart.update({
+            where: { id: cart.id },
+            data: {
+                sum: { increment: quantity }
+            }
+        });
+
+        // cập nhật cart-detail
+        // nếu chưa có -> tạo mới còn có rồi -> cập nhật số lượng
+
+        const currentCartDetail = await prisma.cartDetail.findFirst({
+            where: {
+                productId: productId,
+                cartId: cart.id
+            }
+        });
+
+        await prisma.cartDetail.upsert({
+            where: {
+                id: currentCartDetail ? currentCartDetail.id : 0
+            },
+            update: {
+                quantity: { increment: quantity },
+            },
+            create: {
+                price: product?.price || 0,
+                quantity: quantity,
+                productId: productId,
+                cartId: cart.id
+            },
+        })
+
+        /* Đánh dấu: đoạn code cũ
+        // cập nhật cart-detail
+        const cartDetail = await prisma.cartDetail.findFirst({
+            where: {
+                cartId: cart.id,
+                productId: productId
+            }
+        });
+        // nếu chưa có -> tạo mới còn có rồi -> cập nhật số lượng
+        if (cartDetail) {
+            // cập nhật số lượng sản phẩm trong giỏ hàng
+            await prisma.cartDetail.update({
+                where: { id: cartDetail.id },
+                data: {
+                    quantity: cartDetail.quantity + quantity
+                }
+            });
+        } else {
+            // thêm sản phẩm mới vào giỏ hàng
+            await prisma.cartDetail.create({
+                data: {
+                    cartId: cart.id,
+                    productId: productId,
+                    price: product?.price || 0,
+                    quantity: quantity
+                }
+            });
+        }
+        */
+
     } else {
         // create new cart and add item
         await prisma.cart.create({
